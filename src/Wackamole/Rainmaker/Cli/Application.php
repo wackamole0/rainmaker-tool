a546aad85a58563f13e48486c6e3c6d7ebd1684b
Adam Dowling <adamd@deeson.co.uk>
2015-03-07 13:45:46 +0000

    - Setup framework skeleton

---
+++ src/Wackamole/Rainmaker/Cli/Application.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Wackamole\Rainmaker\Cli;
+
+use Symfony\Component\Console\Application as ParentApplication;
+use Symfony\Component\Console\Helper\FormatterHelper;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\StreamOutput;
+use Symfony\Component\Console\Shell;
+
+class Application extends ParentApplication {
+
+  protected $output;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct()
+  {
+    parent::__construct('Rainmaker CLI', '1.x-dev');
+
+    $this->setDefaultTimezone();
+
+    $this->addCommands($this->getCommands());
+
+    $this->setDefaultCommand('welcome');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getDefaultInputDefinition()
+  {
+    return new InputDefinition(array(
+      new InputArgument('command', InputArgument::REQUIRED, 'The command to execute'),
+      new InputOption('--help', '-h', InputOption::VALUE_NONE, 'Display this help message'),
+      new InputOption('--version', '-V', InputOption::VALUE_NONE, 'Display this application version'),
+    ));
+  }
+
+  /**
+   * @return \Symfony\Component\Console\Command\Command[]
+   */
+  protected function getCommands()
+  {
+    $commands = array();
+    $commands[] = new Command\WelcomeCommand();
+    return $commands;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function getHelp()
+  {
+    $messages = array(
+      $this->getLongVersion(),
+      '',
+      '<comment>Global options:</comment>',
+    );
+
+    foreach ($this->getDefinition()->getOptions() as $option) {
+      $messages[] = sprintf('  %-29s %s %s',
+        '<info>--'.$option->getName().'</info>',
+        $option->getShortcut() ? '<info>-'.$option->getShortcut().'</info>' : '  ',
+        $option->getDescription()
+      );
+    }
+
+    return implode(PHP_EOL, $messages);
+  }
+
+  /**
+   * Set the default timezone.
+   *
+   * PHP 5.4 has removed the autodetection of the system timezone,
+   * so it needs to be done manually.
+   * UTC is the fallback in case autodetection fails.
+   */
+  protected function setDefaultTimezone() {
+    $timezone = 'UTC';
+    if (is_link('/etc/localtime')) {
+      // Mac OS X (and older Linuxes)
+      // /etc/localtime is a symlink to the timezone in /usr/share/zoneinfo.
+      $filename = readlink('/etc/localtime');
+      if (strpos($filename, '/usr/share/zoneinfo/') === 0) {
+        $timezone = substr($filename, 20);
+      }
+    } elseif (file_exists('/etc/timezone')) {
+      // Ubuntu / Debian.
+      $data = file_get_contents('/etc/timezone');
+      if ($data) {
+        $timezone = trim($data);
+      }
+    } elseif (file_exists('/etc/sysconfig/clock')) {
+      // RHEL/CentOS
+      $data = parse_ini_file('/etc/sysconfig/clock');
+      if (!empty($data['ZONE'])) {
+        $timezone = trim($data['ZONE']);
+      }
+    }
+
+    date_default_timezone_set($timezone);
+  }
+
+}
---
+++ src/Wackamole/Rainmaker/Cli/Command/WelcomeCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wackamole\Rainmaker\Cli\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class WelcomeCommand extends Command
+{
+
+  protected function configure()
+  {
+    $this
+      ->setName('welcome')
+      ->setDescription('Welcome to Rainmaker');
+  }
+
+  public function isEnabled()
+  {
+    global $argv;
+    return !isset($argv[1]) || $argv[1] != 'list';
+  }
+
+  public function isLocal()
+  {
+    return TRUE;
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $output->writeln("Welcome to Rainmaker!");
+
+    $application = $this->getApplication();
+
+    $output->writeln("Type <info>rainmaker list</info> to see all available commands.");
+  }
+
+}
