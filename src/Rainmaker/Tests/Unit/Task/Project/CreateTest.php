561038749f9c6860c31cdf62720b97b8748af0ad
Adam Dowling <adamd@deeson.co.uk>
2015-07-15 18:44:04 +0100

    - Fixed paths in the unit test

--- src/Rainmaker/Tests/Unit/Task/Project/CreateTest.php
+++ src/Rainmaker/Tests/Unit/Task/Project/CreateTest.php
@@ -48,20 +48,21 @@ class CreateTest extends \PHPUnit_Framework_TestCase
     $this->assertEquals('00:16:3e:e0:5c:c3', $task->getContainer()->getLxcHwAddr());
     $this->assertEquals('/var/lib/lxc/test/rootfs', $task->getContainer()->getLxcRootFs());
 
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/lxc/lxc_config'), $filesystemMock->getFileContents('/var/lib/lxc/' . $task->getContainer()->getName() . '/config'));
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/lxc/hostname'), $filesystemMock->getFileContents('/var/lib/lxc/' . $task->getContainer()->getName() . '/rootfs/etc/hostname'));
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/lxc/hosts'), $filesystemMock->getFileContents('/var/lib/lxc/' . $task->getContainer()->getName() . '/rootfs/etc/hosts'));
+    $pathToTestCheckFiles = dirname(__FILE__) . '/../../../..';
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/lxc/lxc_config'), $filesystemMock->getFileContents('/var/lib/lxc/' . $task->getContainer()->getName() . '/config'));
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/lxc/hostname'), $filesystemMock->getFileContents('/var/lib/lxc/' . $task->getContainer()->getName() . '/rootfs/etc/hostname'));
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/lxc/hosts'), $filesystemMock->getFileContents('/var/lib/lxc/' . $task->getContainer()->getName() . '/rootfs/etc/hosts'));
 
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/dhcp/host_localdev.test.cluster.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/dhcp/dhcpd.host.conf.d/localdev.test.cluster.conf'));
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/dhcp/dhcpd.host.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/dhcp/dhcpd.host.conf'));
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/dhcp/class_localdev.test.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/dhcp/dhcpd.class.conf.d/localdev.test.conf'));
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/dhcp/dhcpd.class.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/dhcp/dhcpd.class.conf'));
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/dhcp/subnet_10.100.0.0.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/dhcp/dhcpd.subnet.conf.d/10.100.0.0.conf'));
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/dhcp/host_localdev.test.cluster.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/dhcp/dhcpd.host.conf.d/localdev.test.cluster.conf'));
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/dhcp/dhcpd.host.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/dhcp/dhcpd.host.conf'));
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/dhcp/class_localdev.test.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/dhcp/dhcpd.class.conf.d/localdev.test.conf'));
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/dhcp/dhcpd.class.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/dhcp/dhcpd.class.conf'));
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/dhcp/subnet_10.100.0.0.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/dhcp/dhcpd.subnet.conf.d/10.100.0.0.conf'));
 
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/bind/db.test.localdev'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/bind/db.rainmaker/db.test.localdev'));
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/bind/db.10.100.1'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/bind/db.rainmaker/db.10.100.1'));
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/bind/named.test.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/bind/named.conf.rainmaker/test.conf'));
-    $this->assertEquals(file_get_contents(dirname(__FILE__) . '/../../../Resources/tests/bind/named.conf.local'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/bind/named.conf.local'));
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/bind/db.test.localdev'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/bind/db.rainmaker/db.test.localdev'));
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/bind/db.10.100.1'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/bind/db.rainmaker/db.10.100.1'));
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/bind/named.test.conf'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/bind/named.conf.rainmaker/test.conf'));
+    $this->assertEquals(file_get_contents($pathToTestCheckFiles . '/Resources/tests/bind/named.conf.local'), $filesystemMock->getFileContents('/var/lib/lxc/services/rootfs/etc/bind/named.conf.local'));
   }
 
   protected function createDummyProject()
